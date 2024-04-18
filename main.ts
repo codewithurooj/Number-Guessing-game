85af1021ec2fb2b2c794b1566468292a1eaa1ca8
codewithurooj <fatimaurooj152@gmail.com>
2024-04-18 09:00:30 -0700

    Adding Number Guessing Game

---
+++ main.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+console.log("Welcome to codewithurooj - CLI Number guessing game");
+
+const randomNumber = Math.floor(Math.random()*10 +1);
+
+const answer =await inquirer.prompt([
+    {
+        name: "userguessingNumber",
+        type: "number",
+        message:"Enter your guess number(Number limit from 1 to 10):",
+    },
+]);
+if (answer.userguessingNumber ===randomNumber){
+    console.log("Congratulation ! You guess a correct number");
+}
+else{
+    console.log("Sorry, Your guess is wrong!");
+}
